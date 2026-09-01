cabdbf17b286f701c7ce1bc24a8eb9626fcc5ede
kadekadhi120 <kadekadhi120@gmail.com>
2026-09-01 16:44:05 +0700

    Added AdminControler and Enum

--- cbt_be/Models/ResponseModels/Admin/GetActivityLogsResponse.cs
+++ cbt_be/Models/ResponseModels/Admin/GetActivityLogsResponse.cs
@@ -2,7 +2,17 @@
 {
     public class GetActivityLogsResponse
     {
+        public List<ActivityLogDto> Data { get; set; } = new List<ActivityLogDto>();
+        //public int TotalPages { get; set; }
+        //public int CurrentPage { get; set; }
+        //public int UnreadCount { get; set; }
+    }
 
-
+    public class ActivityLogDto
+    {
+        public Guid Id { get; set; }
+        public string Type { get; set; }
+        public string Message { get; set; }
+        public DateTime OccuratedAt { get; set; }
     }
 }
--- cbt_be/Program.cs
+++ cbt_be/Program.cs
@@ -1,5 +1,11 @@
-using Microsoft.EntityFrameworkCore;
+using cbt.be.Validator.Admin;
+
 using cbt.entity;
+using cbt.entity.Models;
+using FluentValidation;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Npgsql;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -7,8 +13,35 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+// 1. Ambil Connection String
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
+// 2. Siapkan NpgsqlDataSourceBuilder SEBELUM mendaftarkan DbContext
+var dataSourceBuilder = new NpgsqlDataSourceBuilder(connectionString);
+dataSourceBuilder.EnableUnmappedTypes();
+
+// PENTING: MapEnum harus dipanggil SEBELUM dataSourceBuilder.Build()
+dataSourceBuilder.MapEnum<ActivityType>("public.activity_type");
+dataSourceBuilder.MapEnum<AttemptStatus>("public.attempt_status");
+dataSourceBuilder.MapEnum<ExamStatus>("public.exam_status");
+dataSourceBuilder.MapEnum<QuestionType>("public.question_type");
+dataSourceBuilder.MapEnum<SubmitType>("public.submit_type");
+dataSourceBuilder.MapEnum<UserRole>("public.user_role");
+dataSourceBuilder.MapEnum<UserStatus>("public.user_status");
+dataSourceBuilder.MapEnum<ViolationType>("public.violation_type");
+
+var dataSource = dataSourceBuilder.Build();
+
+// 3. Daftarkan DbContext HANYA SEKALI menggunakan dataSource yang sudah dikonfigurasi
 builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+    options.UseNpgsql(dataSource));
+
+// 4. Daftarkan FluentValidation dan MediatR
+builder.Services.AddValidatorsFromAssemblyContaining<GetActivityLogsValidator>();
+
+builder.Services.AddMediatR(cfg => {
+    cfg.RegisterServicesFromAssembly(typeof(Program).Assembly);
+});
 
 var app = builder.Build();
 
@@ -19,9 +52,6 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
-
 app.Run();
