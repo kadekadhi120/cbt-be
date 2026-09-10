2423ee5fca26255df5dbde91da33242fdb3fe7a8
kadekadhi120 <kadekadhi120@gmail.com>
2026-09-10 18:30:01 +0700

    added logic create packet

---
+++ cbt_be/Models/RequestModels/Admin/ManagementUjian/CreatePacketUjianRequest.cs
@@ -0,0 +1,15 @@
+﻿using cbt.be.Models.ResponseModels;
+using cbt.be.Models.ResponseModels.Admin.ManagementUjian;
+using MediatR;
+using System.Reflection;
+
+namespace cbt.be.Models.RequestModels.Admin.ManagementUjian
+{
+    public class CreatePacketUjianRequest : IRequest<MainResponse<bool>>
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public int Time{ get; set; }
+        public List<string> ClassCode { get; set; }
+    }
+}
---
+++ cbt_be/RequestHandler/Admin/ManagementUjian/CreatePacketUjianRequestHandler.cs
@@ -0,0 +1,10 @@
+﻿using cbt.be.Models.ResponseModels;
+using MediatR;
+
+namespace cbt.be.RequestHandler.Admin.ManagementUjian
+{
+    public class CreatePacketUjianRequestHandler : IRequestHandler<CreatePacketUjianRequest, MainResponse<bool>>
+    {
+        public readonly 
+    }
+}
